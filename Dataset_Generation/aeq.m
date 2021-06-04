a25d58c7ec9a75888f80e814ae92ba585cf5c1ea
anoncode46 <85317930+anoncode46@users.noreply.github.com>
2021-06-03 20:05:45 -0400

    Update aeq.m

--- Dataset_Generation/aeq.m
+++ Dataset_Generation/aeq.m
@@ -2,6 +2,7 @@
 %
 % Compare two matrices up to a tolerance, tol.
 % Return true if they are equivalent.
+% From Kemp and Tenenbaum, 2008.
 function r = aeq(x,y,tol)
     if nargin < 3
        tol = eps*10^10; %which is 2.2204e-06
