01484cdcf7841bcd2d126197822eafc73ba9fd68
anoncode46 <85317930+anoncode46@users.noreply.github.com>
2021-06-03 20:12:17 -0400

    Update aeq.m

--- Dataset_Generation/aeq.m
+++ Dataset_Generation/aeq.m
@@ -1,8 +1,9 @@
+% adopted from : Lake, B. M., Lawrence, N. D., and Tenenbaum, J. B. (2018). The emergence of organizing structure in conceptual representation. 
+% Cognitive Science, 42(S3), 809-832.
 % function: approximately equal
 %
 % Compare two matrices up to a tolerance, tol.
 % Return true if they are equivalent.
-% From Kemp and Tenenbaum, 2008.
 function r = aeq(x,y,tol)
     if nargin < 3
        tol = eps*10^10; %which is 2.2204e-06
