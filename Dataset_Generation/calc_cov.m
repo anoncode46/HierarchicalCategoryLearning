10d2231a31e90f3f38a7d67d3632f4ce58e9b07d
anoncode46 <85317930+anoncode46@users.noreply.github.com>
2021-06-03 20:12:52 -0400

    Update calc_cov.m

--- Dataset_Generation/calc_cov.m
+++ Dataset_Generation/calc_cov.m
@@ -1,9 +1,10 @@
+% adopted from : Lake, B. M., Lawrence, N. D., and Tenenbaum, J. B. (2018). The emergence of organizing structure in conceptual representation. 
+% Cognitive Science, 42(S3), 809-832.
 % Calculate the covariance matrix, given raw data
 % Data should be n objects by m features, returns the object by object
 % covariance
 %
 % Y is n x n
-% From Kemp and Tenenbaum, 2008.
 function Y = calc_cov(data)
     [n,m] = size(data); %n objects by m features
     %Y = (1/m).*data*data';
